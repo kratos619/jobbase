09933267fa6389428736c16af8bc887dff4a1011
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-27 12:49:09 +0530

    Find and create category

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -2,17 +2,54 @@
 
 namespace app\controllers;
 
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\Category;
+use yii\data\Pagination;
+
 class CategoryController extends \yii\web\Controller
 {
     
     public function actionIndex()
     {
-        return $this->render('index');
+        $query = Category::find();
+        $pagination = New Pagination([
+            'defaultPageSize' => 20,
+            'totalCount' => $query->count(),
+        ]);
+        $category = $query->orderBy('name')
+        ->offset($pagination->offset)
+        ->limit($pagination->limit)
+        ->all();
+
+        return $this->render('index',[
+            'categories' => $category,
+            'pagination' => $pagination
+        ]);
     }
     
     public function actionCreate()
     {
-        return $this->render('create');
+        $category = new Category();
+
+        if ($category->load(Yii::$app->request->post())) {
+            if ($category->validate()) {
+                // form inputs are valid, do something here
+                // save record to db
+
+                $category->save();
+                Yii::$app->getSession()->setFlash('success','Category Is Added');
+                return $this->redirect('index.php?r=/category');
+            }
+        }
+
+            return $this->render('create', [
+                'category' => $category,
+            ]);
+      //  return $this->render('create');
     }
 
 
