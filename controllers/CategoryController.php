83d262de3834717a8e6986cf3cef8ac38af97946
kratos619 <gaurav.palaspagar@hotmail.com>
2018-10-27 07:23:06 +0530

    genrate controller and view Category

---
+++ controllers/CategoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\controllers;
+
+class CategoryController extends \yii\web\Controller
+{
+    
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+    
+    public function actionCreate()
+    {
+        return $this->render('create');
+    }
+
+
+}
